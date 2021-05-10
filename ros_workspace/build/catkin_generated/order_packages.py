1a9a2f100cda6b5011977f631663987103f72657
AlbertRasinski <63299016+AlbertRasinski@users.noreply.github.com>
2021-05-10 22:39:39 +0200

    v1

---
+++ ros_workspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/mateusz/ros_workspace/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/mateusz/ros_workspace/devel;/opt/ros/noetic'.split(';') if '/home/mateusz/ros_workspace/devel;/opt/ros/noetic' != '' else []
